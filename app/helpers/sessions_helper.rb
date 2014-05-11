24aa17b35584ee4918c8ae6096acf8aabf2c4add
Elijah Hsu <xu_c@live.cn>
2014-05-11 08:54:27 -0400

    Finish user edit, update, index and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,6 +14,10 @@ module SessionsHelper
     @current_user = user
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def current_user
     remember_token = User.hash(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
@@ -24,5 +28,14 @@ module SessionsHelper
       User.hash(User.new_remember_token))
     self.current_user = nil
     cookies.delete(:remember_token)
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.fullpath if request.get?
   end
 end
